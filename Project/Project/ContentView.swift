fd1283d10aebee9da45eafa4e4ff7c0fb4f4ea6a
jack <568159877@qq.com>
2020-04-03 17:12:24 +0800

    ok

---
+++ Project/Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project
+//
+//  Created by jack on 2020/4/3.
+//  Copyright © 2020 ProjectToGitHub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, WorldProjectToGitHubProjectToGitHub!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
